4e8ac6ee924ad0d35a89555482feb53ffaf2c716
Aron Berezkin <aron.berezkin@gmail.com>
2022-11-02 15:02:25 +0100

    fix: navigationReset typescript args

--- src/navigation/utils.ts
+++ src/navigation/utils.ts
@@ -5,17 +5,22 @@ import type { ResetState, RootStackParamList } from 'src/navigation/types'
 
 export const navigationRef = createNavigationContainerRef()
 
+// if screen has extra parameter object, require it, else keep it optional
+// https://stackoverflow.com/a/69410398/11447104
 export const navigationReset = <TRoute extends keyof RootStackParamList>(
-  route: TRoute,
-  // if screen has extra parameter object, require it, else keep it optional
-  ...params: RootStackParamList[TRoute] extends object ? [RootStackParamList[TRoute]] : []
-) =>
+  ...args: RootStackParamList[TRoute] extends object
+    ? [route: TRoute, params: RootStackParamList[TRoute]]
+    : [route: TRoute]
+) => {
+  const [route, params] = args
+
   navigationRef.dispatch(
     CommonActions.reset({
       index: 0,
       routes: [{ name: route, params }],
     }),
   )
+}
 
 // use to have a screen to go back to
 export const navigationResetState = (resetState: ResetState) =>
