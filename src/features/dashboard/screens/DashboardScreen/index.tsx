20dfce6416a39db4e509aa27e6a962207f7f63b0
Petr Konecny <petr.konecny@strv.com>
2025-05-14 18:58:24 +0200

    chore: removed env variable

--- src/features/dashboard/screens/DashboardScreen/index.tsx
+++ src/features/dashboard/screens/DashboardScreen/index.tsx
@@ -6,7 +6,6 @@ import { styles } from './styles'
 export const DashboardScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{`${process.env.EXPO_PUBLIC_TEST}`}</Text>
       <Image source="strv_logo" contentFit="contain" style={styles.image} />
     </View>
   )
