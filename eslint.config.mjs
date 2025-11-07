702ebd62a06e985f81f13a07e6687e9135425eca
Alexander Khodyrev <alexander.khodyrev@strv.com>
2025-11-07 14:50:19 +0100

    chore: update testing configuration and dependencies
    - Added a new testing configuration for React Native to ESLint.
    - Updated package dependencies for testing-library from react-hooks to react-native.
    - Updated pnpm lockfile to reflect changes in dependencies.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'eslint/config'
 import reactNative from '@strv/eslint-config-react-native'
+import testingLibrary from 'eslint-plugin-testing-library'
+
 
 /** @type {import("eslint").Linter.Config} */
 const project = {
   rules: {},
 }
 
-export default defineConfig([reactNative, project])
+const testConfig = {
+  files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  ...testingLibrary.configs['flat/react'],
+}
+
+export default defineConfig([reactNative, project, testConfig])
