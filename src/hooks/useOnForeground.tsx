d1b88475217be8ecc66204aa128f9fa46ad03fe6
Alexander Khodyrev <alexander.khodyrev@strv.com>
2026-03-06 16:07:26 +0100

    fix: useOnForeground duplicate listener and unnecessary state

--- src/hooks/useOnForeground.tsx
+++ src/hooks/useOnForeground.tsx
@@ -1,29 +1,24 @@
-import { useState, useEffect } from 'react'
-import { type AppStateStatus } from 'react-native'
-import { AppState } from 'react-native'
+import { useEffect, useRef } from 'react'
+import { AppState, type AppStateStatus } from 'react-native'
 
 import { useLatestValue } from '~/hooks/useLatest'
 
 export function useOnForeground(onForeground: () => void, includeFirstLoad = false) {
-  const [appState, setAppState] = useState(AppState.currentState)
-
-  const latestAppState = useLatestValue(appState)
+  const appState = useRef(AppState.currentState)
   const latestOnForeground = useLatestValue(onForeground)
 
   useEffect(() => {
     const handleAppStateChange = (nextAppState: AppStateStatus) => {
-      if (latestAppState.current.match(/inactive|background/u) && nextAppState === 'active') {
+      if (appState.current.match(/inactive|background/u) && nextAppState === 'active') {
         latestOnForeground.current()
       }
-      setAppState(nextAppState)
+      appState.current = nextAppState
     }
 
     if (includeFirstLoad) latestOnForeground.current()
 
     const subscription = AppState.addEventListener('change', handleAppStateChange)
-    AppState.addEventListener('change', handleAppStateChange)
 
     return () => subscription.remove()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, [includeFirstLoad, latestOnForeground])
 }
